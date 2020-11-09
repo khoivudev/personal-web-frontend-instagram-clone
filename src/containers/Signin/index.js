553ec433f937c08394c8422ae3292deb4f018594
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-11-09 15:18:27 +0700

    sign in request

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -1,9 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "../../helpers/axios";
+import { emailRegex } from "../../helpers/regex";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 import Layout from "../../components/Layout";
 import "./style.css";
 
 const Signin = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateField = () => {
+    if (!emailRegex.test(email)) {
+      M.toast({ html: "Invalid Email", classes: "red darken-3" });
+      return false;
+    }
+    if (!password) {
+      M.toast({ html: "Password required", classes: "red darken-3" });
+      return false;
+    }
+    return true;
+  };
+
+  const handleBtnSignInClick = () => {
+    if (!validateField()) return;
+    axios
+      .post("/auth/signin", { email, password })
+      .then((res) => {
+        if (res.status === 200) {
+          M.toast({
+            html: "Logged in Successfully",
+            classes: "green darken-1",
+          });
+          history.push("/");
+        }
+      })
+      .catch((error) => {
+        M.toast({ html: error.response.data.error, classes: "red darken-3" });
+      });
+  };
+
   return (
     <Layout>
       <div className="my-card">
@@ -11,15 +48,26 @@ const Signin = () => {
           <h2 className="text-logo">Instagram</h2>
           <div className="row">
             <div className="input-field col s12">
-              <input type="text" placeholder="Email" />
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <input type="password" placeholder="Password" />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
-          <button className="btn waves-effect waves-light blue darken-2">
+          <button
+            className="btn waves-effect waves-light blue darken-2"
+            onClick={handleBtnSignInClick}
+          >
             Sign in
           </button>
           <h6>
