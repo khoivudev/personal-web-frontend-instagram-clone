973ee89794b2af0b15be9429d12bf974258b8675
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-11-11 21:36:55 +0700

    get posts in home page & profile page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,45 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../../helpers/axios";
 import Layout from "../../components/Layout";
+import M from "materialize-css";
 import "./style.css";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("/post/my")
+      .then((res) => {
+        if (res.status === 200) {
+          setPosts(res.data.posts);
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        M.toast({ html: "Something went wrong", classes: "red darken-3" });
+      });
+  }, []);
+
   return (
     <Layout>
+      {console.log("asdasd")}
       <div className="home">
-        <div className="card home-card">
-          <h6 className="home-card-title">vu_huy_khoi</h6>
-          <div className="card-image">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1604646358243-a4ea77920d3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="card-content">
-            <i className="material-icons" style={{ color: "red" }}>
-              favorite
-            </i>
-            <h6>Vote vote vote!</h6>
-            <p>Vote for Donal J Trump!</p>
-            <input type="text" placeholder="add a comment" />
-          </div>
-        </div>
-        <div className="card home-card">
-          <h6 className="home-card-title">vu_huy_khoi</h6>
-          <div className="card-image">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1604646358243-a4ea77920d3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="card-content">
-            <i className="material-icons" style={{ color: "red" }}>
-              favorite
-            </i>
-            <h6>Vote vote vote!</h6>
-            <p>Vote for Donal J Trump!</p>
-            <input type="text" placeholder="add a comment" />
-          </div>
-        </div>
+        {!isLoading && (
+          <>
+            {posts.length > 0 ? (
+              posts.map((post) => (
+                <div key={post._id}>
+                  <div className="card home-card">
+                    <h6 className="home-card-title">{`${post.postedBy.firstName} ${post.postedBy.lastName}`}</h6>
+
+                    <div className="card-image">
+                      <img alt="" src={post.photo} />
+                    </div>
+                    <div className="card-content">
+                      <i className="material-icons" style={{ color: "red" }}>
+                        favorite
+                      </i>
+                      <h6>{post.title}</h6>
+                      <p>{post.content}</p>
+                      <input type="text" placeholder="add a comment" />
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="empty">Let's having fun by sharing your things</p>
+            )}
+          </>
+        )}
       </div>
     </Layout>
   );
--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "../../helpers/axios";
 import Layout from "../../components/Layout";
+import M from "materialize-css";
 import "./style.css";
+import { AuthContext } from "../../contexts/auth.context";
 
 const Profile = () => {
+  const { auth } = useContext(AuthContext);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/post/my")
+      .then((res) => {
+        if (res.status === 200) {
+          setPosts(res.data.posts);
+        }
+      })
+      .catch((error) => {
+        M.toast({ html: "Something went wrong", classes: "red darken-3" });
+      });
+  }, []);
+
   return (
     <Layout>
       <div className="profile">
         <div className="profile-info">
           <div className="profile-info-avatar">
-            <img src="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80" />
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80"
+            />
           </div>
           <div>
-            <h4>Elisa Sweety</h4>
+            <h4>{`${auth.user.firstName} ${auth.user.lastName}`}</h4>
             <div className="profile-info-amount">
               <h6>50 posts</h6>
               <h6>50 followers</h6>
@@ -20,48 +42,13 @@ const Profile = () => {
           </div>
         </div>
         <div className="profile-gallery">
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1543877087-ebf71fde2be1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1601758125997-67e236238ab0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1591035897819-f4bdf739f446?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
-          <div className="profile-gallery-item">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1542596594-649edbc13630?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            />
-          </div>
+          {posts.length > 0
+            ? posts.map((post) => (
+                <div className="profile-gallery-item" key={post._id}>
+                  <img alt="" src={post.photo} />
+                </div>
+              ))
+            : null}
         </div>
       </div>
     </Layout>
